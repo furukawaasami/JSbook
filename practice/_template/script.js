d1eaad24dd3458354c8a3b5bfa60e9d46d4759a8
furukawaasami <98880369+furukawaasami@users.noreply.github.com>
2022-05-31 18:00:02 +0900

    practice~218PAGE

--- practice/_template/script.js
+++ practice/_template/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const lang = document.querySelector('html').lang;
+
+if (lang === 'ja') {
+    document.querySelector('option[value="5-2_ja.html"]').selected = true;
+} else if(lang ==='en') {
+    document.querySelector('option[value="5-2_en.html"]').selected = true;
+} else if(lang === 'zh') {
+    document.querySelector('option[value="5-2_zh.html"]').selected = true;
+}
+
 document.getElementById('form').select.onchange = function() {
     location.href = document.getElementById('form').select.value;
 }
